7ec2f5b0c9e6ef23b6564138a7f9187ad0e6f88d
u564030 Pablo M. Ibarrola <pablo.m.ibarrola@accenture.com>
2019-06-06 11:31:30 -0300

    Comit en el branch de desarrollo

---
+++ ProyectoParaGit/src/ClaseDePrueba.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+/**
+ * @author u564030 Pablo M. Ibarrola
+ *
+ */
+public class ClaseDePrueba {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+}
